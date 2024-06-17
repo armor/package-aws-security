86d641e47e79ca18eb3e7ef5d288bb5a764d545e
Sharon Traineanu <sharon.traineanu@armor.com>
2024-06-17 13:38:31 -0500

    CSV-134: Replace quantum with armor in terraform modules

--- examples/aws-cloudtrail-organization/aws-org-cloudtrail/main.tf
+++ examples/aws-cloudtrail-organization/aws-org-cloudtrail/main.tf
@@ -45,7 +45,7 @@ locals {
 
   default_tags = {
     managed_by = "terraform"
-    module     = "quantum-sec/package-aws-security/examples/aws-cloudtrail"
+    module     = "armor/package-aws-security/examples/aws-cloudtrail"
   }
   tags = merge(local.default_tags, var.tags)
 }
